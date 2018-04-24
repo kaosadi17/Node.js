c65f45cee135b36641dfef566ca1127df1d269bc
amalio <kaosadi17@gmail.com>
2018-04-24 17:36:34 +0530

    create simple server program using express

--- express.js
+++ express.js
@@ -1,3 +1,14 @@
 var express = require('express');
 
 var app = express();
+
+app.get('/hello',function(request,response){
+	response.send("Helo world");
+});
+
+app.listen('8080',function(){
+	console.log('app is listening on port 8080');
+});
+
+
+//to run this in browser -> localhost:8080/hello
