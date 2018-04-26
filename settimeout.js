0f68bdba8b26b3718dac64ce8a3a9552922064a1
amalio <kaosadi17@gmail.com>
2018-04-26 10:29:32 +0530

    added settimeout function

---
+++ settimeout.js
@@ -0,0 +1,15 @@
+function first(){
+    console.log('first');
+}
+
+function second(){
+    console.log('second');
+}
+
+function third(){
+    console.log('third');
+}
+
+first();
+setTimeout(second,1000);
+third();
